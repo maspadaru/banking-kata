12dcc03f98b67ad6772c9674b175077e2a9d03b6
Mike Spadaru <mspadaru@gmail.com>
2025-08-22 13:45:21 +0200

    implement fuctionality for deposit method

--- src/main/java/org/mspadaru/banking/account/service/DefaultAccount.java
+++ src/main/java/org/mspadaru/banking/account/service/DefaultAccount.java
@@ -3,21 +3,28 @@ package org.mspadaru.banking.account.service;
 import org.mspadaru.banking.account.api.Account;
 import org.mspadaru.banking.account.output.StatementPrinter;
 import org.mspadaru.banking.account.service.model.Transaction;
+import org.mspadaru.banking.account.service.model.TransactionType;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DefaultAccount implements Account {
 
-    private StatementPrinter statementPrinter;
-    private List<Transaction> transactions;
+    private final StatementPrinter statementPrinter;
+    private final List<Transaction> transactions;
+    private long balance;
 
     public DefaultAccount(StatementPrinter statementPrinter) {
         this.statementPrinter = statementPrinter;
+        this.transactions = new ArrayList<>();
     }
 
     @Override
     public void deposit(int amount) {
-
+        balance += amount;
+        Transaction transaction = new Transaction(LocalDateTime.now(), amount, balance, TransactionType.DEPOSIT);
+        transactions.add(transaction);
     }
 
     @Override
---
+++ src/main/java/org/mspadaru/banking/account/service/model/TransactionType.java
@@ -0,0 +1,5 @@
+package org.mspadaru.banking.account.service.model;
+
+public enum TransactionType {
+    DEPOSIT, WITHDRAW
+}
